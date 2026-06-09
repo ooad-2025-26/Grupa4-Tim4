88a535429f377e4506f49e280e911e82ea013b4f
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2026-06-10 01:06:43 +0200

    implementiran pregled kurseva, konverzije i pretraga valuta

---
+++ ETFPay/ETFPay/Migrations/20260609115409_UklonjenKursIzBaze.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETFPay.Migrations
+{
+    /// <inheritdoc />
+    public partial class UklonjenKursIzBaze : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kurs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IznosZaJedanUSD = table.Column<double>(type: "float", nullable: false),
+                    Valuta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VrijemeAzuriranja = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kurs", x => x.Id);
+                });
+        }
+    }
+}
